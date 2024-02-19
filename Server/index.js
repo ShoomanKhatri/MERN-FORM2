d47bdc40ac6b2230fe6301348e70b41bfcfe299f
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-02-19 13:29:52 +0545

    modified

--- Server/index.js
+++ Server/index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["POST", "GET"],
-    credentials: true,
+    // methods: ["POST", "GET"],
+    // credentials: true,
   })
 );
 app.use(express.json());
